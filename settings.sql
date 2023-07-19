b3a21314d7ec1936550677a5006d5eac8c9ae01d
Ethan Flatt <ethanflatt1@gmail.com>
2023-07-19 11:22:57 -0500

    New file structure

--- settings.sql
+++
@@ -1,4 +0,0 @@
--- settings.sql
-CREATE DATABASE ticketexpert;
-CREATE USER ticketexpertuser WITH PASSWORD 'ticketexpert';
-GRANT ALL PRIVILEGES ON DATABASE ticketexpert TO ticketexpertuser;
--- ticketexpert/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TicketexpertConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ticketexpert'
